4191db7c46ef0fc366f9e616b9509136366a5cd9
annhoang191 <hquynhanh19@gmail.com>
2019-01-31 11:34:43 +0700

    Follow user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :load_user, only: %i(show edit update destroy)
-  before_action :logged_in_user, only: %i(index edit update destroy)
+  before_action :logged_in_user, only: %i(index edit update destroy following followers)
   before_action :correct_user,   only: %i(edit update)
   before_action :admin_user,     only: %i(destroy)
 
@@ -47,6 +47,21 @@ class UsersController < ApplicationController
       flash[:danger] = t ".fail"
       redirect_to users_url
     end
+  end
+
+
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render "show_follow"
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render "show_follow"
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,15 @@ Rails.application.routes.draw do
   get  "/about", to: "static_pages#about"
   get  "/contact", to: "static_pages#contact"
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: %i(edit)
   resources :password_resets, only: %i(new create edit update)
   resources :microposts, only: %i(create destroy)
+  resources :relationships,       only: %i(create destroy)
 
   get    "/login", to: "sessions#new"
   post   "/login", to: "sessions#create"
